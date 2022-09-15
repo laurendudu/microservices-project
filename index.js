d43ff6b09cd845ecf53ed3e4a8d6b0cf5b94b5ad
laurendudu <lauren.durivault@gmail.com>
2022-09-15 15:42:23 +0200

    Setting up static files using express middleware function

--- index.js
+++ index.js
@@ -5,17 +5,30 @@ const port = 3000
 const {readFileSync, promises: fsPromises} = require('fs');
 
 
+// Main endpoint
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 
+// Random word endpoint
+app.get('/word/', (req, res) => {
+  words = ReadWords('data/liste_francais_utf8.txt')
+  randomWord = generateRandomWord(words)
+  res.send(randomWord)
+})
+
+// Setup static files
+app.use('/static', express.static('public'))
+
+
+// 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
 
-
+// Read word array
 function ReadWords(filename) {
   const contents = readFileSync(filename, 'utf-8');
 
@@ -24,13 +37,13 @@ function ReadWords(filename) {
   return arr;
 }
 
-
+// Generate random index
 const randomIndex = () => {
   const date = new Date();
   return (date.getFullYear() * date.getDate() * (date.getMonth() + 1)) % words.length
 }
 
-
+// Generate random word from random index
 function generateRandomWord(array) {
   word = array[randomIndex()]
   console.log(word)
@@ -39,10 +52,5 @@ function generateRandomWord(array) {
 }
 
 
-words = ReadWords('data/liste_francais_utf8.txt')
 
 
-app.get('/word/', (req, res) => {
-  randomWord = generateRandomWord(words)
-  res.send(randomWord)
-})
