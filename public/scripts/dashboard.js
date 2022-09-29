2db629bb0eb79f0ccad32ff2a99e6897f837a60d
laurendudu <lauren.durivault@gmail.com>
2022-09-29 18:26:36 +0200

    Finishing score and average

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,6 +1,8 @@
 const backGame = document.getElementById('backGame-btn');
 const score_html = document.getElementById("score")
+const avg_html = document.getElementById("avg")
 var score = localStorage.getItem("score")
+var avg = localStorage.getItem("avg")
 
 
 // redirect user back to game
@@ -12,24 +14,34 @@ backGame.addEventListener('click', (event) => {
 function updateScore() {
     
     var score = localStorage.getItem("score")
+    var avg = localStorage.getItem("avg")
 
-    if (score){
+    if (score) {
         score ++;
         localStorage.setItem("score", score)
 
-    } else{
+        avg = (((score - 1) * avg) + (6 - numberOfGuesses + 1)) / score
+        localStorage.setItem("avg", avg)
+
+    } else {
         score = 1;
+        avg = 6 - numberOfGuesses + 1
+        console.log(numberOfGuesses)
         localStorage.setItem("score", score)
+        localStorage.setItem("avg", avg)
     }
 
 }
 
 // display score to html
-if (score){
-    score_html.innerHTML = score
-
-} else{
-    score_html.innerHTML = "0";
+if (score) {
+    score_html.innerHTML = `Number of words guessed: ${score}`;
+} else {
+    score_html.innerHTML = "Number of words guessed: 0";
 }
 
-
+if (avg) {
+    avg_html.innerHTML = `Average number of guesses: ${avg}`;
+} else {
+    avg_html.innerHTML = "Average number of guesses: 0"
+}
